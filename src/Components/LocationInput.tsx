08f96cafa1fe119761b913fd42605a34655a46a3
Fay <samsongreen72@gmail.com>
2022-05-19 11:24:27 -0400

    LocationInput component gets current location from browser or user input then makes an api request for current weather.

--- src/Components/LocationInput.tsx
+++ src/Components/LocationInput.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import useEffectOnce from '../hooks/useEffectOnce';
+import {CurrentWeatherResponse } from '../Types';
 
-export const LocationInput:React.FC= () => {
+interface LocationInputProps {
+  currentWeather: CurrentWeatherResponse;
+  setCurrentWeather: (currentWeatherObj: CurrentWeatherResponse) => void;
+}
+
+export const LocationInput:React.FC<LocationInputProps>= ({ currentWeather, setCurrentWeather }) => {
   
   const [location, setLocation] = useState("");
   const [browserLoc, setBrowserLoc] = useState<any>({});
@@ -23,7 +29,7 @@ export const LocationInput:React.FC= () => {
   const fetchCurrent = () => {
     fetch(`${baseUrl}/current.json?key=${key}&q=${location}`)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => setCurrentWeather(data))
   }
 
   const handleChange = (e: React.ChangeEvent<any>, setFn: React.Dispatch<React.SetStateAction<any>>) => {
@@ -41,69 +47,3 @@ export const LocationInput:React.FC= () => {
 }
 
 // make component to show weather when page loads with browser location
-// Object { location: {…}, current: {…} }
-// ​
-// current: Object { last_updated_epoch: 1652818500, last_updated: "2022-05-17 16:15", temp_c: 28.9, … }
-// ​​
-// cloud: 50
-// ​​
-// condition: Object { text: "Partly cloudy", icon: "//cdn.weatherapi.com/weather/64x64/day/116.png", code: 1003 }
-// ​​​
-// code: 1003
-// ​​​
-// icon: "//cdn.weatherapi.com/weather/64x64/day/116.png"
-// ​​​
-// text: "Partly cloudy"
-// ​​​
-// <prototype>: Object { … }
-// ​​
-// feelslike_c: 29.1
-// ​​
-// feelslike_f: 84.4
-// ​​
-// gust_kph: 15.8
-// ​​
-// gust_mph: 9.8
-// ​​
-// humidity: 30
-// ​​
-// is_day: 1
-// ​​
-// last_updated: "2022-05-17 16:15"
-// ​​
-// last_updated_epoch: 1652818500
-// ​​
-// precip_in: 0
-// ​​
-// precip_mm: 0
-// ​​
-// pressure_in: 29.99
-// ​​
-// pressure_mb: 1016
-// ​​
-// temp_c: 28.9
-// ​​
-// temp_f: 84
-// ​​
-// uv: 7
-// ​​
-// vis_km: 16
-// ​​
-// vis_miles: 9
-// ​​
-// wind_degree: 280
-// ​​
-// wind_dir: "W"
-// ​​
-// wind_kph: 11.2
-// ​​
-// wind_mph: 6.9
-// ​​
-// <prototype>: Object { … }
-// ​
-// location: Object { name: "Port Fulton", region: "Indiana", country: "United States of America", … }
-// ​
-// <prototype>: Object { … }
-// LocationInput.tsx:24
-
-​
