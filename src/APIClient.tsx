edae410281102a63808730d603069a1228164e23
Fay <samsongreen72@gmail.com>
2022-05-26 15:18:50 -0400

    Currently API fetch()  functions to fetch current weather and three day forecast.

--- src/APIClient.tsx
+++ src/APIClient.tsx
@@ -1,6 +1,7 @@
 const baseUrl = 'http://api.weatherapi.com/v1';
 const key = '397acc8ee0e44011a76162011221305';
 
+
 export const fetchCurrentWeather = (location: string) => {
   const data = fetch(`${baseUrl}/current.json?key=${key}&q=${location}`)
   .then((res) => res.json())
@@ -14,8 +15,14 @@ export const fetchCurrentWeatherViaBrowser = (browserLoc: any) => {
   return data
 }
 
-export const fetchForecast = (location: any) => {
-  const data = fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${location}&days=7`)
+export const fetchForecastViaLocation = (location: any) => {
+  const data = fetch(`${baseUrl}/forecast.json?key=${key}&q=${location}&days=3`)
+  .then((res) => res.json())
+  return data
+}
+
+export const fetchForecastViaBrowser = (browserLoc: any) => {
+  const data = fetch(`${baseUrl}/forecast.json?key=${key}&q=${browserLoc?.coords?.latitude},${browserLoc?.coords?.longitude}&days=3`)
   .then((res) => res.json())
   return data
 }
